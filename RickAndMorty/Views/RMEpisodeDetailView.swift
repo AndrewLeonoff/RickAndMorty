3a73978d65fe68194e824e044649ec9d411e20cc
Andrew Leonov <andrewleonov999@gmail.com>
2023-07-01 23:37:58 +0300

    API Cache Layer

---
+++ RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Andrew Leonov on 01.07.2023.
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+    
+}
