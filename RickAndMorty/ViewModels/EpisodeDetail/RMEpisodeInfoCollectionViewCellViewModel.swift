2b673cb24a399a3f7038b644a83a25537c6c4fcc
Andrew Leonov <andrewleonov999@gmail.com>
2023-07-04 02:27:32 +0300

    Episode Detail View creation

---
+++ RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeInfoCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RMEpisodeInfoCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Andrew Leonov on 04.07.2023.
+//
+
+import Foundation
+
+struct RMEpisodeInfoCollectionViewCellViewModel {
+    public let title: String
+    public let value: String
+}
---
+++ RickAndMorty/Views/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMEpisodeInfoCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Andrew Leonov on 04.07.2023.
+//
+
+import UIKit
+
+final class RMEpisodeInfoCollectionViewCell: UICollectionViewCell {
+    
+}
