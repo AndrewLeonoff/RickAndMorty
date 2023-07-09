b35434ff04443eef4057bbbeb2718ae370d8d683
Andrew Leonov <andrewleonov999@gmail.com>
2023-07-09 21:03:27 +0300

    Creation of Setting Screen Item ViewModels

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Andrew Leonov on 09.07.2023.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
