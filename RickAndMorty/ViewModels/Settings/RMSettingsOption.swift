9ff5c90b2ddabff3d98c5a98dfd2840faa526090
Andrew Leonov <andrewleonov999@gmail.com>
2023-07-10 21:59:23 +0300

    Setting Options updates

--- RickAndMorty/ViewModels/Settings/RMSettingsOption.swift
+++ RickAndMorty/ViewModels/Settings/RMSettingsOption.swift
@@ -9,27 +9,18 @@ import UIKit
 
 enum RMSettingsOption: CaseIterable {
     case rateApp
-    case contactUs
-    case terms
-    case privacy
+    case contactMe
     case apiReference
-    case viewSeries
     case viewCode
     
     var targetUrl: URL? {
         switch self {
         case .rateApp:
             return nil
-        case .contactUs:
-            return URL(string: "https://github.com/AndrewLeonoff")
-        case .terms:
-            return URL(string: "https://github.com/AndrewLeonoff")
-        case .privacy:
+        case .contactMe:
             return URL(string: "https://github.com/AndrewLeonoff")
         case .apiReference:
             return URL(string: "https://rickandmortyapi.com/documentation")
-        case .viewSeries:
-            return URL(string: "https://www.youtube.com/playlist?list=PL5PR3UyfTWvdl4Ya_2veOB6TM16FXuv4y")
         case .viewCode:
             return URL(string: "https://github.com/AndrewLeonoff/RickAndMorty")
         }
@@ -39,16 +30,10 @@ enum RMSettingsOption: CaseIterable {
         switch self {
         case .rateApp:
             return "Rate App"
-        case .contactUs:
-            return "Contact Us"
-        case .terms:
-            return "Terms of Service"
-        case .privacy:
-            return "Privacy Policy"
+        case .contactMe:
+            return "Contact Me"
         case .apiReference:
             return "API Reference"
-        case .viewSeries:
-            return "View Video Series"
         case .viewCode:
             return "View App Code"
         }
@@ -58,16 +43,10 @@ enum RMSettingsOption: CaseIterable {
         switch self {
         case .rateApp:
             return .systemBlue
-        case .contactUs:
+        case .contactMe:
             return .systemGreen
-        case .terms:
-            return .systemRed
-        case .privacy:
-            return .systemYellow
         case .apiReference:
             return .systemOrange
-        case .viewSeries:
-            return .systemPurple
         case .viewCode:
             return .systemPink
         }
@@ -77,16 +56,10 @@ enum RMSettingsOption: CaseIterable {
         switch self {
         case .rateApp:
             return UIImage(systemName: "star.fill")
-        case .contactUs:
+        case .contactMe:
             return UIImage(systemName: "paperplane")
-        case .terms:
-            return UIImage(systemName: "doc")
-        case .privacy:
-            return UIImage(systemName: "lock")
         case .apiReference:
             return UIImage(systemName: "list.clipboard")
-        case .viewSeries:
-            return UIImage(systemName: "tv.fill")
         case .viewCode:
             return UIImage(systemName: "hammer.fill")
         }
